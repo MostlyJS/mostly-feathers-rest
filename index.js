8d1c29346690f8081cccfe239de9c214cbf66927
Fora <luoxuemai@boqii.com>
2017-07-24 16:59:39 +0800

    Add profiler middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports = require('./lib/index');
+module.exports.profiler = require('./lib/profiler');
 module.exports.localStorage = require('./lib/upload/local_storage');
---
+++ src/profiler.js
@@ -0,0 +1,12 @@
+export default function profiler() {
+  return function(req, res, next) {
+    const tag = '[' + new Date().toISOString() + '] gateway profiler => ' + req.method + '  ' + req.url;
+    console.time(tag);
+    // The 'finish' event comes from core Node.js, it means Node is done handing
+    // off the response headers and body to the underlying OS.
+    res.on('finish', () => {
+      console.timeEnd(tag);
+    });
+    next();
+  };
+}
