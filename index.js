27cef6fd228bc7412b6901ebb548ad22a0734555
forafish <fora.luo@gmail.com>
2017-08-07 13:02:42 +0800

    move localStorage to mostly-multer-storages

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports = require('./lib/index');
 module.exports.profiler = require('./lib/profiler');
-module.exports.localStorage = require('./lib/upload/local_storage');
